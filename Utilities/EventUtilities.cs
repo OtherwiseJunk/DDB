5c5503dd5a7dc60e20ff0e0d5ad0c0e3affd0d1b
Bobby David <RobertWarrenDavid@gmail.com>
2022-06-13 19:58:40 -0400

    Rename Event Utilities Move Event Utilities to Utilities namespace Bump Version

--- Utilities/EventUtilities.cs
+++ Utilities/EventUtilities.cs
@@ -8,18 +8,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace DartsDiscordBots.Handlers
+namespace DartsDiscordBots.Utilities
 {
-    public class EventHandlers
+    public class EventUtilities
     {
         public static string EventUrlFormat = @"https://discord.com/events/{0}/{1}";
         public static void AnnounceNewEvent(SocketGuildEvent arg)
         {
-            ITextChannel announcementChnl = (ITextChannel) arg.Guild.Channels.FirstOrDefault(c => c.Name.ToLower() == "announcements");
+            ITextChannel announcementChnl = (ITextChannel)arg.Guild.Channels.FirstOrDefault(c => c.Name.ToLower() == "announcements");
             SocketGuildUser user = arg.Creator;
             Console.WriteLine($"[DDB] - An event has been created! Announcement Channel null? {announcementChnl == null}. Creating user null? {user == null}");
-            if(announcementChnl != null)
-            {                
+            if (announcementChnl != null)
+            {
                 string eventMessage = $"{BotUtilities.GetDisplayNameForUser(user)} has created a new event!{Environment.NewLine}{Environment.NewLine}{String.Format(EventUrlFormat, arg.Guild.Id, arg.Id)}";
                 announcementChnl.SendMessageAsync(eventMessage);
             }
@@ -31,16 +31,22 @@ namespace DartsDiscordBots.Handlers
             if (announcementChnl != null)
             {
                 string channelNameString = arg.Channel != null ? $" Join {arg.Channel.Name} now!" : String.Empty;
-                string mentionString = "";
-                await foreach (var users in arg.GetUsersAsync(RequestOptions.Default))
-                {
-                    foreach (RestUser user in users) {
-                        mentionString += user.Mention;
-                    }                    
-                }
+                string mentionString = await GetInterestedUsersMentioned(arg);
                 string eventMessage = $"{arg.Name} event has started!{channelNameString}{mentionString}";
                 _ = announcementChnl.SendMessageAsync(eventMessage);
             }
+        }
+        public static async Task<string> GetInterestedUsersMentioned(IGuildScheduledEvent guildScheduledEvent)
+        {
+            string mentionString = "";
+            await foreach (var users in guildScheduledEvent.GetUsersAsync(RequestOptions.Default))
+            {
+                foreach (RestUser user in users)
+                {
+                    mentionString += user.Mention;
+                }
+            }
+            return mentionString;
         }
     }
 }
