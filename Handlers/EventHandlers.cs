27aff95ae75620b8d56f8f3439ca90fddd563bc7
Bobby David <RobertWarrenDavid@gmail.com>
2022-06-13 17:04:10 -0400

    Add logging to event handlers

--- Handlers/EventHandlers.cs
+++ Handlers/EventHandlers.cs
@@ -16,20 +16,25 @@ namespace DartsDiscordBots.Handlers
         public static void AnnounceNewEvent(SocketGuildEvent arg)
         {
             ITextChannel announcementChnl = (ITextChannel) arg.Guild.Channels.FirstOrDefault(c => c.Name.ToLower() == "announcements");
+            SocketGuildUser user = arg.Creator;
+            Console.WriteLine($"[DDB] - An event has been created! Announcement Channel null? {announcementChnl == null}. Creating user null? {user == null}");
             if(announcementChnl != null)
             {                
-                string eventMessage = $"{BotUtilities.GetDisplayNameForUser(arg.Creator)} has created a new event!{Environment.NewLine}{Environment.NewLine}{String.Format(EventUrlFormat, arg.Guild.Id, arg.Id)}";
+                string eventMessage = $"{BotUtilities.GetDisplayNameForUser(user)} has created a new event!{Environment.NewLine}{Environment.NewLine}{String.Format(EventUrlFormat, arg.Guild.Id, arg.Id)}";
                 announcementChnl.SendMessageAsync(eventMessage);
             }
         }
         public static async void AnnounceNewEventStarted(SocketGuildEvent arg)
         {
-            ITextChannel announcementChnl = (ITextChannel)arg.Guild.Channels.FirstOrDefault(c => c.Name.ToLower() == "announcements");            
+            ITextChannel announcementChnl = (ITextChannel)arg.Guild.Channels.FirstOrDefault(c => c.Name.ToLower() == "announcements");
+            Console.WriteLine($"[DDB] - An event has started! Announcement Channel null? {announcementChnl == null}.");
             if (announcementChnl != null)
             {
                 string channelNameString = arg.Channel != null ? $" Join {arg.Channel.Name} now!" : String.Empty;
                 string mentionString = "";
-                foreach (RestUser user in arg.GetUsersAsync(RequestOptions.Default).ToListAsync().Result)
+                List<RestUser> users = (List<RestUser>)arg.GetUsersAsync(RequestOptions.Default);
+                Console.WriteLine($"[DDB] - Users list null? {users == null}. Users list empty? {users.Count == 0}");
+                foreach (var user in users)
                 {
                     mentionString += user.Mention;
                 }
